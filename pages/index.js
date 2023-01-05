7650e89e42e5af44a80774e5b7030c69336500ea
Carlos8621 <carlosordazrodriguez2@gmail.com>
2023-01-05 11:13:24 -0600

    camaraaaaa

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,13 @@ import { Button,Segment } from 'semantic-ui-react'
 export default function Home() {
   return (
     <div>
+      <center>
       <Segment inverted>
-      <Button inverted color='red'>hola mundo</Button>
+      <Button inverted color='red'>Camara papito</Button>
+      <Button inverted color='red'>Ya quiero</Button>
+      <Button inverted color='red'>Hacer la Base de Datos</Button>
       </Segment>
+      </center>
     </div>
   )
 }
